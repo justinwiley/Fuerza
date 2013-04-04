5800c20ced84de261505dc2dc860dea600be9f84
Justin Wiley <justin.wiley@gmail.com>
2013-04-04 18:57:24 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuerza::Application.config.secret_token = '64844786c4c086cb9495d3af000d521d4ffcfd6b6636875b965959c38bde9f02e70a7bfa70aee39549cc21bd046c72e36a8daedea006c16f5e324914888b48ee'
